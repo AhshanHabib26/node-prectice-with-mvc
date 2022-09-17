93907519bfaae79d2cb279258222e7001b5a7669
AhshanHabib26 <habib.net27@gmail.com>
2022-09-17 07:17:49 +0600

    Product Schema Create

---
+++ models/product.schema.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+// Product Schema Create
+
+const ProductSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: [3, "Title Name Minimum Three Characters"],
+      maxLength: [100, "Title Name Maximum Three Characters"],
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        // If You Have Predefined Data Then Use  Enum
+        values: ["kg", "litre", "pcs"],
+        meassage: "unit value can't {VALUE}, must be kg/litre/pcs",
+      },
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+      validate: {
+        validator: (value) => {
+          // Custom Validatior
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      message: "Quntity is Must be an Integer",
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["in-stock", "out-of-stock", "discontinued"],
+        meassage: "Status can't be {VALUE}",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+// Product Model Create
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
